ed8f110ddbeda58bd4a4d787ff7f1bb497b621e6
Michael J. Ryan <tracker1@gmail.com>
2015-02-16 10:02:52 -0700

    add .gitignore for node projects, tweak package.json to include npm modules used, adjust gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,20 @@ var minifyCSS = require('gulp-minify-css');
  
 // CSS concat, auto-prefix and minify
 gulp.task('css', function() {
-  gulp.src(['./lib/littlebox.css'])
+  return gulp.src(['./lib/littlebox.css'])
     .pipe(concat('littlebox.min.css'))
     .pipe(autoprefix('last 2 versions'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('./build/'));
 });
 
-// default gulp task
-gulp.task('default', ['css'], function() {
+// watch task
+gulp.task('watch', ['css'], function() {
   // watch for CSS changes
-  gulp.watch('./lib/littlebox.css', function() {
-	gulp.run('css');
+  return gulp.watch('./lib/**', function() {
+  	gulp.run('css');
   });
 });
+
+//default gulp task
+gulp.task('default', ['css']);
